d2e6e119c73559ac908a731becd23d8980f82239
Stefan Pauka <paukastefan@gmail.com>
2021-03-21 16:34:21 +0100

    update demo

--- examples/webapp-cra/src/Todo/reducer/state.ts
+++ examples/webapp-cra/src/Todo/reducer/state.ts
@@ -2,6 +2,7 @@ import { TodoData } from "../types/TodoData";
 
 export interface State {
   todos: TodoData[];
-  nullableTags: (string | number)[] | null;
-  undefinableNumbers: number[] | undefined;
+  // If you want, you may give messing around with nullables a try ;)
+  // nullableTags: (string | number)[] | null;
+  // undefinableNumbers: number[] | undefined;
 }
