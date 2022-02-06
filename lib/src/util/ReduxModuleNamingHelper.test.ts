cba7d2e84a8ceaae03023eaf9df6b56d6ea06f17
Stefan Pauka <derfrodo0715@gmail.com>
2022-02-06 21:19:09 +0100

    add test

--- lib/src/util/ReduxModuleNamingHelper.test.ts
+++ lib/src/util/ReduxModuleNamingHelper.test.ts
@@ -35,4 +35,43 @@ describe("ReduxModuleNamingHelper tests", () => {
             expect(result).toMatch(/(\/\/.*$)|(\n$)/g);
         });
     });
+
+    describe("ReduxModuleNamingHelper.addGeneratedHeader", () => {
+        it("ReduxModuleNamingHelper.addGeneratedHeader returns adds generator File info", async () => {
+            // arrange:
+            const testoptions: ReduxModuleNamingHelperOptions = {
+                addFeatureAsActionPrefix: true,
+            };
+
+            const clazz = new ReduxModuleNamingHelper(testoptions, undefined);
+            const startFileLine = clazz.getGeneralGenertedFileInformation();
+            const overwriteWarning = clazz.getDoNotOverwriteWarning();
+
+            // act
+            const result = clazz.addGeneratedHeader("TESTCONTENT");
+
+            // assert
+            expect(result).toMatch(/((\/\/.*$)|(\n$))*TESTCONTENT/);
+            expect(result.indexOf(startFileLine)).toBeGreaterThan(-1);
+            expect(result.indexOf(overwriteWarning)).toBeGreaterThan(-1);
+        });
+        it("ReduxModuleNamingHelper.addGeneratedHeader returns adds generator File info and omits 'do not overwrite info'", async () => {
+            // arrange:
+            const testoptions: ReduxModuleNamingHelperOptions = {
+                addFeatureAsActionPrefix: true,
+            };
+
+            const clazz = new ReduxModuleNamingHelper(testoptions, undefined);
+            const startFileLine = clazz.getGeneralGenertedFileInformation();
+            const overwriteWarning = clazz.getDoNotOverwriteWarning();
+
+            // act
+            const result = clazz.addGeneratedHeader("TESTCONTENT", true);
+
+            // assert
+            expect(result).toMatch(/((\/\/.*$)|(\n$))*TESTCONTENT/);
+            expect(result.indexOf(startFileLine)).toBeGreaterThan(-1);
+            expect(result.indexOf(overwriteWarning)).toBeLessThan(0);
+        });
+    });
 });
