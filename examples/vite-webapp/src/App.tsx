61128167e51b460e85f84d6513af63cf70d275f1
Stefan <stefan@lenovo14.local>
2025-12-08 21:22:12 +0100

    add another example.

---
+++ examples/vite-webapp/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <>
+      <div className="App" data-testid="app-container">
+        <main>
+          Hello World
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
